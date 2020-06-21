422c532e4f40eb0ffc5cc56f0f761b80327017b7
Heitor Quaglia <heitor.bq@hotmail.com>
2020-06-21 12:25:46 -0300

    add service 'Categoria'

---
+++ src/main/java/br/quaglia/cursomc/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package br.quaglia.cursomc.services;
+
+import br.quaglia.cursomc.domain.Categoria;
+import br.quaglia.cursomc.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repository;
+
+    public Categoria buscar(Integer id){
+        Optional<Categoria> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
