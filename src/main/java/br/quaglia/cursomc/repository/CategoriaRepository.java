8df7ca9db410352d875fd4e6761b02868fb4b0ae
Heitor Quaglia <heitor.bq@hotmail.com>
2020-06-21 12:25:20 -0300

    implementing repository interface 'Categoria'

--- src/main/java/br/quaglia/cursomc/repository/CategoriaRepository.java
+++ src/main/java/br/quaglia/cursomc/repository/CategoriaRepository.java
@@ -1,4 +1,10 @@
 package br.quaglia.cursomc.repository;
 
-public class CategoriaRepository {
+import br.quaglia.cursomc.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>  {
+
 }
