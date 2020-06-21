32f4507a2abd2c8c029d43d645589900a8c65e82
Heitor Quaglia <heitor.bq@hotmail.com>
2020-06-20 21:44:41 -0300

    add mapping for 'Categoria'

--- src/main/java/br/quaglia/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/quaglia/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,20 @@
 package br.quaglia.cursomc.resources;
 
+import br.quaglia.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "REST está funcionando";
+    public List<Categoria> listar() {
+
+        return null;
     }
 
 }
