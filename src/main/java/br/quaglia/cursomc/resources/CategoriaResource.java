4b9d3402050306b200f6cdc03b64d7f56b25db20
Heitor Quaglia <heitor.bq@hotmail.com>
2020-06-21 12:46:28 -0300

    add 'buscar' for 'Categoria'

--- src/main/java/br/quaglia/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/quaglia/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,10 @@
 package br.quaglia.cursomc.resources;
 
 import br.quaglia.cursomc.domain.Categoria;
+import br.quaglia.cursomc.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +15,20 @@ import java.util.List;
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
+    @Autowired
+    private CategoriaService service;
+
     @RequestMapping(method = RequestMethod.GET)
     public List<Categoria> listar() {
 
         return null;
+    }
+
+    @RequestMapping(value="/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> buscar(@PathVariable Integer id){
+        Categoria obj = service.buscar(id);
+
+        return ResponseEntity.ok().body(obj);
     }
 
 }
