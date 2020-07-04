4884fd3a6cfe420196054168f6b46513b12d2ed1
Heitor Quaglia <heitor.bq@hotmail.com>
2020-07-04 17:54:57 -0300

    fix throws Exception

--- src/main/java/br/quaglia/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/quaglia/cursomc/resources/CategoriaResource.java
@@ -2,6 +2,7 @@ package br.quaglia.cursomc.resources;
 
 import br.quaglia.cursomc.domain.Categoria;
 import br.quaglia.cursomc.services.CategoriaService;
+import javassist.tools.rmi.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +26,7 @@ public class CategoriaResource {
     }
 
     @RequestMapping(value="/{id}", method = RequestMethod.GET)
-    public ResponseEntity<?> buscar(@PathVariable Integer id){
+    public ResponseEntity<?> buscar(@PathVariable Integer id) throws ObjectNotFoundException {
         Categoria obj = service.buscar(id);
 
         return ResponseEntity.ok().body(obj);
