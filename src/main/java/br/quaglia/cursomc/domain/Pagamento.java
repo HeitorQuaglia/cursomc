7a4d2de5b2c2ba3646b219fadc99d3aec9209fcf
Heitor Quaglia <heitor.bq@hotmail.com>
2020-07-04 22:01:02 -0300

    add Pedido & Pagamento

---
+++ src/main/java/br/quaglia/cursomc/domain/Pagamento.java
@@ -0,0 +1,67 @@
+package br.quaglia.cursomc.domain;
+
+import br.quaglia.cursomc.domain.enums.EstadoPagamento;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Pagamento implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private EstadoPagamento estado;
+
+    private Pedido pedido;
+
+    public Pagamento() {
+    }
+
+    public Pagamento(Integer id, EstadoPagamento estado, Pedido pedido) {
+        this.id = id;
+        this.estado = estado;
+        this.pedido = pedido;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public EstadoPagamento getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoPagamento estado) {
+        this.estado = estado;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pagamento)) return false;
+        Pagamento pagamento = (Pagamento) o;
+        return getId().equals(pagamento.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
---
+++ src/main/java/br/quaglia/cursomc/domain/enums/EstadoPagamento.java
@@ -0,0 +1,34 @@
+package br.quaglia.cursomc.domain.enums;
+
+public enum EstadoPagamento {
+
+    PENDENTE(1, "Pendente"),
+    QUITADO(2, "Quitado"),
+    CANCELADO(3, "Cancelado");
+
+    private int codigo;
+    private String descricao;
+
+    EstadoPagamento(int codigo, String descricao) {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public static EstadoPagamento toEnum(Integer codigo) {
+        if (codigo == null) return null;
+
+        for (EstadoPagamento x : EstadoPagamento.values()) {
+            if (codigo.equals(x.getCodigo())) {
+                return x;
+            }
+        }
+
+        throw new IllegalArgumentException("Id " + codigo + " inválido.");
+    }
+}
